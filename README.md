ddb3d06c4a41d7335cb7c7be36fcff09f740e4a9
Dominik Śliwiński <dominiksliwek@gmail.com>
2022-01-24 01:06:33 +0100

    Stable version

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-Spotify downloader
+# About 
+
+Spotify downloader is a free extensions to Google Chrome that allows you to download music from spotfy using URL
+
+## Installation
+
+You must add extension by hand. Download package and add to currently instlled extensions [here](google://extensions/). If you don't know how to add extension - click [here]() to check tutorial
+
+
+## Usage
+
+Just pass right to start downloading. It (should) store music file in your Downloads directory.
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- index.js
+++ index.js
@@ -6,50 +6,69 @@ function main() {
     // divs in index.html that are used
     var input   = document.querySelector('#url'),
         form    = document.querySelector('#form'),
-        respond = document.querySelector('#respond');
-    
+        text    = document.querySelector(".text"),
+        loader  = document.querySelector("#loader"),
+        download= document.querySelector("p#downloading");
+        error   = document.querySelector("#error"),
+        link    = document.querySelector("a#download");
+
+
     form.addEventListener('submit', function(e){
         e.preventDefault(); 
-
-        // remove error class if last call was unsuccessful
-        respond.classList.remove('error');
-
+        
         // check if content is url or is it empty
         if(!validator(input.value)) return;
-        try{
-            if(!ajaxRequest(input.value)) throw ("cos sie zjebalo");
-        } catch(e){
-            respond.classList.add('error');
-            respond.textContent = e;
-        }
+        
+        // remove error class if last call was unsuccessful
+        text.style.display = 'none';
+        download.style.display = 'none';
+        error.style.display = 'none';
+        loader.style.display = 'block';
+
+        ajaxRequest(input.value);
     });
     
-    // AJAX call
     function ajaxRequest(content){
         // create class for connection
         var Connection = class{
             constructor(){
-                this.url    = 'http://mc.test/test';
-                this.port   = '30000';
-                this.param  = 'url';
-                this.method = 'get';
+                this.path   = 'http://83.8.153.120/tester/';
+                this.port   = '80';
+                this.method = 'GET';
             }
         }
         const   conn    = new Connection(),
-                ajax    = new XMLHttpRequest();
+                ajax    = new XMLHttpRequest(),
+                date    = new Date(),
+                csrf    = date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds();
+                url     = conn.path + "?url=" + content + "&csrf=" + csrf;
 
         ajax.onreadystatechange = function() {
             // on success
             if (this.readyState == 4 && this.status == 200) {
-                respond.textContent = this.responseText;
+                var musicUrl = conn.path + this.responseText;
+                loader.style.display = 'none';
+                download.style.display = 'block';
+                link.setAttribute('href', musicUrl);
+                ((musicUrl) => {
+                    const ahref = document.createElement('a');
+                    ahref.href = musicUrl;
+                    ahref.download = FILE_NAME;
+                    document.body.appendChild(ahref);
+                    ahref.click();
+                    document.body.removeChild(ahref);
+                });
             }
             // on error
-            if(this.status !== 200) {
-                respond.textContent = this.responseText;
-                return;
+            if(this.readyState == 4 && this.status !== 200) {
+                loader.style.display = 'none';
+                error.style.display = 'block';
+                error.textContent = this.responseText;
+                return false;
             }
         };
-        ajax.open(conn.method, conn.url + '?' + conn.param + "=" + content, true);
+        
+        ajax.open(conn.method, url, true);
         ajax.send();
         return true;
     }
@@ -57,18 +76,23 @@ function main() {
     // validate request
     function validator(checkedUrl){
         if(checkedUrl == ''){
-            respond.textContent = "Pass any link!";
-            respond.classList.add('error');
+            text.style.display = 'none';
+            error.style.display = 'block';
+            error.textContent = "Pass any link!";
             return false;
         }
-        if(checkedUrl.indexOf('http://') !== 0){
-            respond.textContent = "Pass right link!";
-            respond.classList.add('error');
+        if(checkedUrl.indexOf('https://open.spotify') !== 0){
+            text.style.display = 'none';
+            error.style.display = 'block';
+            error.textContent = "Pass right spotify link!";
             return false;
         }
         return true;
     }
 
+    // clicking on download button must be handled by js, because otherwise it doesn't open at all
+    link.addEventListener('click', function(){
+        var href = link.getAttribute('href');
+        window.open(href);
+    })
 }
-
-
